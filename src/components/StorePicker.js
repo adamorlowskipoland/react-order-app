c5f9f989bcfe33c38cc2f0bacbcb1751ca2eaf2e
Adam <adamorlowskipoland@icloud.com>
2018-06-12 01:09:02 +0200

    fix: created first components, pass props

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 class StorePicker extends React.Component {
   render() {
-    return <p>I am a store picker!</p>;
+    return (
+      <Fragment>
+        <p>Hello</p>
+        <form action="" className="store-selector">
+          {/* TODO: correct this*/}
+
+          <h2>Please Enter a Store</h2>
+          <input type="text"
+                 required placeholder={"Store Name"}/>
+          <button type="submit">
+            Visit Store
+            <span role='img'
+                  aria-label='pointing right'>👉</span>
+          </button>
+        </form>
+      </Fragment>
+    );
   }
 }
 
