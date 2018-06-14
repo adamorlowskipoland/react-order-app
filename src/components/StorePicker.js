3467585c7b06d41e9a0046fa70e0ae3bb1d66522
Adam Orlowski <aorlows1@intercars.eu>
2018-06-14 16:41:45 +0200

    feat: add goToStore function

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,25 +1,38 @@
-import React, { Fragment } from "react";
+import React, {Fragment} from "react";
+import {getFunName} from "../helpers";
 
 class StorePicker extends React.Component {
-  render() {
-    return (
-      <Fragment>
-        <p>Hello</p>
-        <form action="" className="store-selector">
-          {/* TODO: correct this*/}
+    myInput = React.createRef();
 
-          <h2>Please Enter a Store</h2>
-          <input type="text"
-                 required placeholder={"Store Name"}/>
-          <button type="submit">
-            Visit Store
+    goToStore = (event) => {
+        event.preventDefault();
+        const storeName = this.myInput.value.value;
+        this.props.history.push(`/store/${storeName}`);
+    };
+
+    render() {
+        return (
+            <Fragment>
+                <p>Hello</p>
+                <form onSubmit={this.goToStore}
+                      className="store-selector">
+                    {/* TODO: correct this*/}
+
+                    <h2>Please Enter a Store</h2>
+                    <input type="text"
+                           ref={this.myInput}
+                           placeholder={"Store Name"}
+                           defaultValue={getFunName()}
+                           required/>
+                    <button type="submit">
+                        Visit Store
             <span role='img'
-                  aria-label='pointing right'>👉</span>
-          </button>
-        </form>
-      </Fragment>
-    );
-  }
+                  aria-label='pointing right'> 👉</span>
+                    </button>
+                </form>
+            </Fragment>
+        );
+    }
 }
 
 export default StorePicker;
