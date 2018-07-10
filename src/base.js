836b6aa255dcd13f7c7ac932bd1d6c83f79f38ae
Adam <adamorlowskipoland@icloud.com>
2018-07-11 01:12:19 +0200

    feat: update key

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCwoWECYwN-9s3Qx5-LGVF60WGwRR2zx3g",
-  authDomain: "react-order-app-39cbe.firebaseapp.com",
-  databaseURL: "https://react-order-app-39cbe.firebaseio.com",
+  apiKey: "addYourOwnApiKey",
+  authDomain: "firebaseproject.firebaseapp.com",
+  databaseURL: "https://firebaseproject.firebaseio.com",
 });
 
 const base = Rebase.createClass(firebaseApp.database());
