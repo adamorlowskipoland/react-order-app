262686453f86d5aa700ffdc031a47644e6276878
Adam <adamorlowskipoland@icloud.com>
2018-07-10 23:12:53 +0200

    feat: display order, count total

--- src/components/App.js
+++ src/components/App.js
@@ -8,52 +8,75 @@ import Fish from "./Fish";
 const title = 'Fresh Seafood Market';
 
 class App extends React.Component {
-    // Way to create state before react": "^16.3.0-alpha.1
-    // constructor() {
-    //     super();
-    //     this.state = {}
-    // }
+  // Way to create state before react": "^16.3.0-alpha.1
+  // constructor() {
+  //     super();
+  //     this.state = {}
+  // }
 
-    state = {
-        fishes: {},
-        order: {},
-    };
-    addFish = (fish) => {
-        console.log('adding a fish 🐟')
-        //    1. Take a copy of the exsisting state
-        const fishes = {...this.state.fishes};
-        //    2. Add a new fish 🐟 to that fishes variable
-        fishes[`fish${Date.now()}`] = fish;
-        //    3. Sate the new fishes object to state
-        this.setState({
-            fishes,
-        });
-    };
-    loadSampleFishes = () => {
-        this.setState({
-            fishes: sampleFishes,
-        });
-    };
+  state = {
+    fishes: {},
+    order: {},
+  };
 
-    render() {
-        return (
-            <div className="catch-of-the-day">
-                <div className="menu">
-                    <Header tagline={title}/>
-                    <ul className="fishes">
-                        {
-                            Object.keys(this.state.fishes)
-                                .map(key => <Fish key={key} details={this.state.fishes[key]}>{key}</Fish>)
-                        }
-                    </ul>
-                </div>
-                <Order/>
-                <Inventory
-                    addFish={this.addFish}
-                    loadSampleFishes={this.loadSampleFishes}/>
-            </div>
-        );
-    }
+  addFish = (fish) => {
+    console.log('adding a fish 🐟')
+    //    1. Take a copy of the exsisting state
+    const fishes = { ...this.state.fishes };
+    //    2. Add a new fish 🐟 to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    //    3. Sate the new fishes object to state
+    this.setState({
+      fishes,
+    });
+  };
+
+  loadSampleFishes = () => {
+    this.setState({
+      fishes: sampleFishes,
+    });
+  };
+
+  addToOrder = (key) => {
+    //    1. Take a copy of state
+    const order = { ...this.state.order };
+    //    2. Either add to the order, or update the number in the order
+    order[key] = order[key] + 1 || 1;
+    //      3. Call set state to update out state
+    this.setState({
+      order,
+    })
+  };
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline={title}/>
+          <ul className="fishes">
+            {
+              Object.keys(this.state.fishes)
+                .map(key => (
+                    <Fish
+                      key={key}
+                      index={key}
+                      details={this.state.fishes[key]}
+                      addToOrder={this.addToOrder}
+                    >
+                      {key}
+                    </Fish>
+                  )
+                )
+            }
+          </ul>
+        </div>
+        <Order fishes={this.state.fishes} order={this.state.order}/>
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
-  render() {
+  renderOrderItems = (key) => {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const fishIsAvailable = fish.status === 'available';
+    if (!fishIsAvailable) {
+      return <li key={key}>Sorry {fish ? fish.name : 'fish'} is no longer available</li>;
+    }
     return (
-      <div className="order">
-        order
+      <li key={key}>
+        {count} lbs {fish.name} {formatPrice(count * fish.price)}
+      </li>
+    )
+  };
+
+  render() {
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const fishIsAvailable = fish && fish.status === 'available';
+      if (fishIsAvailable) {
+        return prevTotal + (count * fish.price);
+      } else {
+        return prevTotal;
+      }
+    }, 0);
+
+    return (
+      <div className="order-wrap">
+        <h2>Order</h2>
+        <ul className="order">
+          {orderIds.map(this.renderOrderItems)}
+        </ul>
+        <div className="total">Total:
+          <strong> {formatPrice(total)}</strong>
+        </div>
       </div>
     );
   }
